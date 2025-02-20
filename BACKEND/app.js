64d7374d449a0a356f26ad0ddbfc88b344f6d4d0
Anurag <anuragchandra1601@gmail.com>
2025-02-20 21:39:38 +0530

    last one final12

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -77,7 +77,8 @@ const corsOptions = {
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:8000',
-        'http://localhost:3000'
+        'http://localhost:3000',
+        'https://gemini-ai-blush.vercel.app/'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
